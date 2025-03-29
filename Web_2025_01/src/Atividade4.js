c781d1f6573e827411dcedb4d178cbc079b3be0e
EduJobs015 <edualbuq8@gmail.com>
2025-03-29 19:06:12 -0300

    oi

--- Web_2025_01/src/Atividade4.js
+++ Web_2025_01/src/Atividade4.js
@@ -1,14 +1,26 @@
-var numeros1 = [12, 25, 37, 40, 18, 55, 62, 71, 84, 91];
+var numeros1 = [45, 29, 33, 10, 22, 50, 67, 88, 79, 74];
 var impar = [];
 var par = [];
+var armazenar = numeros1[0];
 for (var _i = 0, numeros1_1 = numeros1; _i < numeros1_1.length; _i++) {
     var imparPar = numeros1_1[_i];
     if (imparPar % 2 == 1) {
-        impar.push(imparPar);
+        if (armazenar <= imparPar) {
+            impar.push(imparPar);
+        }
+        else {
+            impar.unshift(imparPar);
+        }
     }
     else {
-        par.push(imparPar);
+        if (armazenar <= imparPar) {
+            par.push(imparPar);
+        }
+        else {
+            par.unshift(imparPar);
+        }
     }
+    armazenar = imparPar;
 }
 console.log(impar);
 console.log(par);
--- Web_2025_01/src/Atividade4.ts
+++ Web_2025_01/src/Atividade4.ts
@@ -1,24 +1,24 @@
-let numeros1: number[] = [12, 25, 37, 40, 18, 55, 62, 71, 84, 91]
+let numeros1: number[] = [45, 29, 33, 10, 22, 50, 67, 88, 79, 74]
 let impar: number[] = []
 let par: number[] = []
-let anterior: number | undefined
+let armazenar: number = numeros1 [0]
+
 for(let imparPar of numeros1){
-    if(imparPar % 2 === 1){
-        if(imparPar >= anterior){
+    if(imparPar % 2 == 1){
+        if(imparPar <= armazenar){
             impar.unshift(imparPar)
         }else{
             impar.push(imparPar)
         }
     }else{
-        if(imparPar >= anterior){
+        if(imparPar <= armazenar){
             par.unshift(imparPar)
         }else{
             par.push(imparPar)
         }
     }
-    anterior = imparPar
+    armazenar = imparPar
 }
-
 console.log(impar)
 console.log(par)
 
