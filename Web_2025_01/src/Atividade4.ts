aa533f3829d766973395aabab7f70a24ef4bc417
EduJobs015 <edualbuq8@gmail.com>
2025-03-30 10:34:24 -0300

    oi

--- Web_2025_01/src/Atividade4.ts
+++ Web_2025_01/src/Atividade4.ts
@@ -2,24 +2,34 @@ let numeros1: number[] =  [12, 25, 37, 40, 18, 55, 62, 71, 84, 91]
 let impar: number[] = []
 let par: number[] = []
 let armazenar: number = 0
+let cont: number = 0 
+let maior: number = 0
+let menor: number = numeros1[0]
 
 for(let imparPar of numeros1){
     if(imparPar % 2 == 1){
-        if(imparPar <= armazenar){
-            impar.unshift(imparPar)
-        }else{
-            impar.push(imparPar)
-        }
+        impar.push(imparPar)
     }else{
-        if(imparPar <= armazenar){
-            par.unshift(imparPar)
-        }else{
-            par.push(imparPar)
+        par.push(imparPar)
         }
+
+    armazenar +=imparPar
+    cont ++
+
+    if(maior < imparPar){
+        maior = imparPar
     }
-        armazenar = imparPar
+    if(menor > imparPar){
+        menor = imparPar
+    }
 }
 
-console.log(impar)
-console.log(par)
+impar.sort((a, b) => a - b) // Ordena os ímpares de forma crescente
+par.sort((a, b) => a - b) 
+
+console.log('Todos os valores impares : ',impar)
+console.log('Todos os valores pares :',par)
+console.log('O valor de todos os numeros somados he de :',armazenar)
+console.log(`A media de ${numeros1} he de : ${armazenar / cont}`)
+console.log(`O maior numero he : ${maior} e o menor numero he : ${menor}`)
 
