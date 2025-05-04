b52563d2d2e9fb56cc30214ac4b26fa5469d62ad
EduJobs015 <edualbuq8@gmail.com>
2025-05-03 21:18:23 -0300

    oi

--- Web_2025_01/src/FunçõesAssincronas/Exercicio1Assincrona.ts
+++ Web_2025_01/src/FunçõesAssincronas/Exercicio1Assincrona.ts
@@ -5,24 +5,33 @@ type Tarefa ={
 }
 
 const Tarefas : Tarefa [] = [
-{ descricao : " Fazer relat ório", prioridade : 2, concluida : false } ,
-{ descricao : " Enviar e- mail ", prioridade : 3 , concluida : false } ,
-{ descricao : " Reuni ão com equipe ", prioridade : 1 , concluida : false }
+{ descricao : " Fazer relatório", prioridade : 2, concluida : false } ,
+{ descricao : " Enviar e-mail ", prioridade : 3 , concluida : false } ,
+{ descricao : " Reunião com a equipe ", prioridade : 1 , concluida : false }
 ]
 
-function imprimirTarefa(descricao: string, indice : number,quantidade : number): void{
+function imprimirTarefa(descricao: string, indice : number,totalTarefas : number): void{
     console.log(`Tarefa concluida : ${descricao}`)
+    console.log(`Progresso: ${indice + 1} / ${totalTarefas}`)
 
-    console.log()
+}
+Tarefas.sort((a:Tarefa, b :Tarefa) => a.prioridade - b.prioridade)
+
+function executarTarefa(tarefas : Tarefa[],  callback : (descricao: string, indice : number, quantidade : number) => void, intervalo:number ): void{
+
+        for (let i = 0; i < tarefas.length; i ++){
+            let timeout = setInterval(() => {
+                console.log(`Execurtando a tarefa ${Tarefas[i].prioridade}...`)
+            }, intervalo)
+            if(Tarefas[i].descricao == "Cancelar"){
+                clearInterval(timeout)
+            }else{
+                Tarefas[i].concluida = true
+                console.log(Tarefas[i])
+            callback(Tarefas[i].descricao, i, 3)
+            }
+            clearInterval(timeout)
+        }
 }
 
-function executarTarefa(descricao: string, prioridade: number, concluida: boolean, callback : (descricao: string, indice : number,quantidade : number)): void{
-    setInterval(() => console.log("Tarefa sendo processada ..."), 2000)
-
-    concluida = true
-
-    console.log(`Tarefa ${descricao} ${prioridade} ${concluida} está concluida !!!`)
-
-
-    setTimeout(() => 1000)
-}
+executarTarefa(Tarefas,imprimirTarefa, 1500)
